681d70523ea5ca9c355a3e65b181f75c339ee144
Olaf Gladis <olaf@gladis.org>
2014-07-31 19:40:40 +0200

    pep8 fixes for tests

--- test_game.py
+++ test_game.py
@@ -1,6 +1,8 @@
 from game import Group, Position, Map
 
+
 class MockTile(object):
+
     def __init__(self, color):
         self.color = color
         self.group = Group([self])
@@ -19,17 +21,18 @@ def test_group_creation_from_tile():
     assert t.color == "white"
     assert t.group.color == "white"
 
+
 def test_group_merge():
     bg = Group(color="black")
     groups = []
     tiles = []
-    for col in ["red","greem","blue","yellow"]:
+    for col in ["red", "greem", "blue", "yellow"]:
         tile = MockTile(col)
         group = tile.group
         tiles.append(tile)
         groups.append(group)
 
-    for t,g in zip(tiles, groups):
+    for t, g in zip(tiles, groups):
         assert t.group == g
 
     groups[0].merge(groups[1])
@@ -39,35 +42,32 @@ def test_group_merge():
     # for t,g in zip(tiles, groups):
         # assert t.group == g
 
+
 def test_position():
-    p = Position(1,1)
-    position_list = list(map(lambda e:e.offset, p.neighbours))
-    for offset_tuple in [(1,0),(2,1),(2,2),(1,2),(0,2),(0,1)]:
+    p = Position(1, 1)
+    position_list = list(map(lambda e: e.offset, p.neighbours))
+    for offset_tuple in [(1, 0), (2, 1), (2, 2), (1, 2), (0, 2), (0, 1)]:
         assert offset_tuple in position_list
     assert len(p.neighbours) == 6
-    p = Position(2,1)
-    position_list = list(map(lambda e:e.offset, p.neighbours))
-    for offset_tuple in [(1,0),(2,0),(3,0),(3,1),(2,2),(1,1)]:
+    p = Position(2, 1)
+    position_list = list(map(lambda e: e.offset, p.neighbours))
+    for offset_tuple in [(1, 0), (2, 0), (3, 0), (3, 1), (2, 2), (1, 1)]:
         assert offset_tuple in position_list
     assert len(p.neighbours) == 6
+
 
 def test_map():
     m = Map(filename="foo")
-    assert m._is_position_valid(Position(2,2))
-    assert m._is_position_valid(Position(15,2))
-    assert m._is_position_valid(Position(16,2)) == False
-    assert m._is_position_valid(Position(15,1))
-    assert m._is_position_valid(Position(16,1)) == False
-    assert m._is_position_valid(Position(0,0))
-    assert m._is_position_valid(Position(-1,0)) == False
-    assert m._is_position_valid(Position(0,-1)) == False
-    assert m._is_position_valid(Position(-1,-1)) == False
-    assert m._is_position_valid(Position(0,11))
-    assert m._is_position_valid(Position(0,12)) == False
-    assert m._is_position_valid(Position(15,12)) == False
-    assert m._is_position_valid(Position(15,11))
-
-
-
-
-
+    assert m._is_position_valid(Position(2, 2))
+    assert m._is_position_valid(Position(15, 2))
+    assert m._is_position_valid(Position(16, 2)) == False
+    assert m._is_position_valid(Position(15, 1))
+    assert m._is_position_valid(Position(16, 1)) == False
+    assert m._is_position_valid(Position(0, 0))
+    assert m._is_position_valid(Position(-1, 0)) == False
+    assert m._is_position_valid(Position(0, -1)) == False
+    assert m._is_position_valid(Position(-1, -1)) == False
+    assert m._is_position_valid(Position(0, 11))
+    assert m._is_position_valid(Position(0, 12)) == False
+    assert m._is_position_valid(Position(15, 12)) == False
+    assert m._is_position_valid(Position(15, 11))
